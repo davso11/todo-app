bcc398bd1aaf1b7985ff01ea8bddca2ac3b0be10
David Sehi <david.sehi2016@gmail.com>
2023-11-13 20:50:53 +0000

    fix main layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,31 @@
-import { View, Text, StyleSheet, useWindowDimensions } from "react-native";
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
 import { Container } from "../components/container";
 import { TodoList } from "../components/todo-list";
-import { BG, MAIN, MAIN_DARK } from "../constants/colors";
+import { BG, MAIN, BORDER } from "../constants/styles";
 import { Button } from "../components/button";
 import { useTodos } from "../hooks/api";
 import { ScreenLoader } from "../components/screen-loader";
 import { dayjs } from "../lib/dayjs";
+// import { DUMMY_TODOS } from "../constants/data";
+
+const { height: deviceHeight } = Dimensions.get("window");
 
 function Home() {
-  const { height } = useWindowDimensions();
-  const { data: todos, status } = useTodos(
-    "c04ea068-22c1-4003-8a7c-f7fd209eb227"
-  );
+  const {
+    data: todos,
+    status,
+    error,
+  } = useTodos("c04ea068-22c1-4003-8a7c-f7fd209eb227");
 
   if (status === "pending") {
     return <ScreenLoader />;
   }
 
   if (status === "error") {
+    console.log(error);
+
     return (
       <View style={styles.center}>
         <Text>Erreur survenue.</Text>
@@ -28,52 +34,52 @@ function Home() {
   }
 
   return (
-    <SafeAreaView>
-      <View style={styles._} />
-      <Container style={[styles.container, { height }]}>
-        {/* TITLE */}
-        <View style={styles["title-box"]}>
-          <Text style={[styles["title-box-text"], styles.date]}>
-            {dayjs(Date.now()).format("ll")}
-          </Text>
-          <Text style={[styles["title-box-text"], styles.title]}>
-            Mes tâches
-          </Text>
-        </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles._} />
 
-        {/* TODOS */}
-        {todos.length > 0 ? (
-          <TodoList
-            todos={todos}
-            style={{
-              width: "100%",
-              flexGrow: 0,
-              maxHeight: 254,
-            }}
-          />
-        ) : (
-          <View style={styles["empty-todo-box"]}>
-            <Text>Aucune tâche enregistrée.</Text>
-          </View>
-        )}
-
-        {/* TODO: COMPLETED TASKS */}
-
-        {/* NAV BUTTON */}
-        <Link href="/new" asChild style={styles["nav-btn"]}>
-          <Button>
-            <Text
-              style={{
-                fontSize: 16,
-                color: "#fff",
-                fontWeight: "600",
-              }}
-            >
-              Nouvelle tâche
+        <Container style={[styles.container]}>
+          {/* HEADER */}
+          <View style={styles["title-box"]}>
+            <Text style={[styles["title-box-text"], styles.date]}>
+              {dayjs(Date.now()).format("ll")}
             </Text>
-          </Button>
-        </Link>
-      </Container>
+            <Text style={[styles["title-box-text"], styles.title]}>
+              Mes tâches
+            </Text>
+          </View>
+
+          {/* TODOS */}
+          {todos.length > 0 ? (
+            <TodoList
+              todos={todos}
+              scrollEnabled={false}
+              style={{
+                width: "100%",
+              }}
+            />
+          ) : (
+            <View style={styles["empty-todo-box"]}>
+              <Text>Aucune tâche enregistrée.</Text>
+            </View>
+          )}
+        </Container>
+      </ScrollView>
+
+      {/* NAV BUTTON */}
+      <Link href="/new" asChild style={styles["nav-btn"]}>
+        <Button>
+          <Text
+            style={{
+              fontSize: 16,
+              color: "#fff",
+              fontWeight: "600",
+            }}
+          >
+            Nouvelle tâche
+          </Text>
+        </Button>
+      </Link>
     </SafeAreaView>
   );
 }
@@ -81,14 +87,18 @@ function Home() {
 const styles = StyleSheet.create({
   _: {
     width: "100%",
-    height: "20%",
-    position: "absolute",
     backgroundColor: MAIN,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: -1,
+    height: Math.max(deviceHeight * 0.3, 250),
   },
   container: {
-    paddingVertical: 24,
+    flex: 1,
+    paddingTop: 24,
+    paddingBottom: 80,
     rowGap: 32,
-    position: "relative",
     alignItems: "center",
   },
   "title-box": {
@@ -113,7 +123,10 @@ const styles = StyleSheet.create({
   },
   "nav-btn": {
     position: "absolute",
-    bottom: 24,
+    bottom: 16,
+    left: 16,
+    right: 16,
+    marginHorizontal: "auto",
   },
   "empty-todo-box": {
     width: "100%",
@@ -123,13 +136,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: BG,
     borderRadius: 12,
-    shadowColor: MAIN_DARK,
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    shadowOffset: {
-      width: 1,
-      height: 1,
-    },
+    ...BORDER,
   },
   center: {
     flex: 1,
