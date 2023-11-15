f1da23bdda0b5d65ba1be412a2fe7822e7bcdba0
David Sehi <david.sehi2016@gmail.com>
2023-11-15 16:21:11 +0000

    from slot to stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MAIN } from "../constants/styles";
 
 const qc = new QueryClient();
 
@@ -8,7 +9,24 @@ function HomeLayout() {
   return (
     <QueryClientProvider client={qc}>
       <SafeAreaProvider>
-        <Slot />
+        <Stack>
+          <Stack.Screen
+            name="index"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="new"
+            options={{
+              headerTitle: "Nouvelle tâche",
+              headerTintColor: "#fff",
+              headerStyle: {
+                backgroundColor: MAIN,
+              },
+            }}
+          />
+        </Stack>
       </SafeAreaProvider>
     </QueryClientProvider>
   );
--- app/new/_layout.tsx
+++
@@ -1,7 +0,0 @@
-import { Slot } from "expo-router";
-
-function Layout() {
-  return <Slot />;
-}
-
-export default Layout;
